07a930036f9477813abde1badb29a75e87ab7136
Maikel de Vries <mdt.maikel@gmail.com>
2016-08-30 19:11:12 +0200

    add room entrance

---
+++ TowerObjects.ocd/Room.ocd/Entrance.ocd/Script.c
@@ -0,0 +1,29 @@
+/**
+	Room Entrance
+	Provides the entrance to a tower room.
+
+	@author Maikel
+*/
+
+
+/*-- Entering --*/
+
+public func Construction()
+{
+	SetEntrance(true);
+	return true;
+}
+
+public func Collection2(object obj)
+{
+	GameCallEx("OnRoomEntranceEntered", obj);
+	return;
+}
+
+
+
+/*-- Properties --*/
+
+local Name = "$Name$";
+local Description = "$Description$";
+local Plane = 200;

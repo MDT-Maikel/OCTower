106e67fb5d6e151cb595bf144501ead985655342
Maikel de Vries <mdt.maikel@gmail.com>
2016-09-01 23:17:57 +0200

    move template scenario scripts to global functions
    This avoids code duplication when rooms are based on the template room.

--- RoomTemplate.ocs/Script.c
+++ RoomTemplate.ocs/Script.c
@@ -1,74 +1,11 @@
 /**
 	Room Template
-	Template scenario which can be used to build tower rooms.
+	Template scenario which can be used to build tower rooms. Do not modify this script.
 	
-	@author <Name Here>
+	@author Tower Team
 */
-
-
-/*-- Room Scripts --*/
 
 public func Initialize()
 {
-	// Create room control object and warn if there exist multiple.
-	var def, index, room_def;
-	while (def = GetDefinition(++index))
-	{
-		if (def->~IsRoom() && def != RoomBase)
-		{
-			if (!room_def)
-				room_def = def;
-			else
-				Log("WARNING: Multiple room control definitions found (%i), creating first found %i", def, room_def);
-		}
-	}
-	if (room_def)
-		CreateObject(room_def);
-	else
-		Log("WARNING: No room control definition found.");
-	
-	// Create basic rules.
-	var rule_restart = CreateObject(Rule_Restart);
-	rule_restart.SaveScenarioObject = Scenario.NoSave;
-	return;
-}
-
-
-public func NoSave() { return true; }
-
-/*-- Room Test --*/
-
-global func TestRoom()
-{
-	// Find room object and test the settings.
-	var def, index, room_def;
-	while (def = GetDefinition(++index))
-	{
-		if (def->~IsRoom() && def != RoomBase)
-		{
-			room_def = def;
-			break;		
-		}
-	}
-	
-	// Log room properties.
-	Log("Your room has name: %s", room_def->GetRoomName());
-	Log("Your room has description: %s", room_def->GetRoomDescription());
-	Log("Your room has author: %s", room_def->GetRoomAuthor());
-	
-	// Check if the author has added a joker both in the room and the settings.
-	// TODO
-	
-	// Check the number of room entrances.
-	if (ObjectCount(Find_ID(RoomEntrance)) == 0)
-		Log("WARNING: Room has no entrances, create one.");
-	else if (ObjectCount(Find_ID(RoomEntrance)) > 1)
-		Log("WARNING: Room has more than one entrances, remove the superfluous ones.");
-		
-	// Check the number of room exits.
-	if (ObjectCount(Find_ID(RoomExit)) == 0)
-		Log("WARNING: Room has no exits, create one.");
-	else if (ObjectCount(Find_ID(RoomExit)) > 1)
-		Log("WARNING: Room has more than one exits, remove the superfluous ones.");
-	return;
+	return InitializeTemplate();
 }
---
+++ TowerObjects.ocd/System.ocg/Template.c
@@ -0,0 +1,65 @@
+// Several functions which are useful in the template scenario.
+
+global func InitializeTemplate()
+{
+	// Create room control object and warn if there exist multiple.
+	var def, index, room_def;
+	while (def = GetDefinition(++index))
+	{
+		if (def->~IsRoom() && def != RoomBase)
+		{
+			if (!room_def)
+				room_def = def;
+			else
+				Log("WARNING: Multiple room control definitions found (%i), creating first found %i", def, room_def);
+		}
+	}
+	if (room_def)
+		CreateObject(room_def);
+	else
+		Log("WARNING: No room control definition found.");
+	
+	// Create basic rules.
+	var rule_restart = CreateObject(Rule_Restart);
+	rule_restart.SaveScenarioObject = Global.NoSave;
+	return;
+}
+
+// To prevent certain objects from being saved for a room.
+global func NoSave() { return true; }
+
+// Run in the template scenario to check all settings.
+global func TestRoom()
+{
+	// Find room object and test the settings.
+	var def, index, room_def;
+	while (def = GetDefinition(++index))
+	{
+		if (def->~IsRoom() && def != RoomBase)
+		{
+			room_def = def;
+			break;		
+		}
+	}
+	
+	// Log room properties.
+	Log("Your room has name: %s", room_def->GetRoomName());
+	Log("Your room has description: %s", room_def->GetRoomDescription());
+	Log("Your room has author: %s", room_def->GetRoomAuthor());
+	
+	// Check if the author has added a joker both in the room and the settings.
+	// TODO
+	
+	// Check the number of room entrances.
+	if (ObjectCount(Find_ID(RoomEntrance)) == 0)
+		Log("WARNING: Room has no entrances, create one.");
+	else if (ObjectCount(Find_ID(RoomEntrance)) > 1)
+		Log("WARNING: Room has more than one entrances, remove the superfluous ones.");
+		
+	// Check the number of room exits.
+	if (ObjectCount(Find_ID(RoomExit)) == 0)
+		Log("WARNING: Room has no exits, create one.");
+	else if (ObjectCount(Find_ID(RoomExit)) > 1)
+		Log("WARNING: Room has more than one exits, remove the superfluous ones.");
+	return;
+}

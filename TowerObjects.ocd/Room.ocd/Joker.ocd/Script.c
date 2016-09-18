f9e0421ff19684932bb297d3548039b0237cc4b1
Maikel de Vries <mdt.maikel@gmail.com>
2016-09-18 10:23:39 +0200

    add joker object

---
+++ TowerObjects.ocd/Room.ocd/Joker.ocd/Script.c
@@ -0,0 +1,16 @@
+/**
+	Joker
+	Collect and deliver to the level entry or exit to free them.
+	Once freed, you can use them to skip a room.
+	
+	@author Sven2, Maikel	
+*/
+
+public func IsJoker() { return true; }
+
+
+/*-- Properties --*/
+
+local Name = "$Name$";
+local Description = "$Description$";
+local Collectible = true;

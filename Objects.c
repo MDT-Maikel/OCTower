512d1d2ae297ca46da983c9fbb6593ca5a36f7d0
Maikel de Vries <mdt.maikel@gmail.com>
2016-09-20 15:37:09 +0200

    update main scenario part

--- Objects.c
+++ Objects.c
@@ -17,81 +17,101 @@ func InitializeObjects()
 	var Grass013 = CreateObjectAbove(Grass, 215, 358);
 	var Grass014 = CreateObjectAbove(Grass, 224, 358);
 	var Grass015 = CreateObjectAbove(Grass, 228, 358);
-	var Grass016 = CreateObjectAbove(Grass, 248, 359);
-	var Grass017 = CreateObjectAbove(Grass, 256, 358);
-	var Grass018 = CreateObjectAbove(Grass, 260, 353);
-	var Grass019 = CreateObject(Grass, 264, 350);
-	var Grass020 = CreateObject(Grass, 268, 346);
+	var Grass016 = CreateObjectAbove(Grass, 250, 357);
+	var Grass017 = CreateObjectAbove(Grass, 258, 356);
+	var Grass018 = CreateObjectAbove(Grass, 262, 351);
+	var Grass019 = CreateObjectAbove(Grass, 238, 357);
+	var Grass020 = CreateObject(Grass, 269, 346);
 	var Grass021 = CreateObject(Grass, 273, 343);
 	var Grass022 = CreateObject(Grass, 280, 342);
 	var Grass023 = CreateObjectAbove(Grass, 283, 338);
 	var Grass024 = CreateObject(Grass, 288, 334);
-	var Grass025 = CreateObjectAbove(Grass, 297, 335);
+	var Grass025 = CreateObject(Grass, 295, 331);
+	Grass025->SetR(-1);
 	var Grass026 = CreateObjectAbove(Grass, 302, 335);
 	var Grass027 = CreateObjectAbove(Grass, 138, 357);
-	var Grass028 = CreateObjectAbove(Grass, 248, 358);
-	var Grass029 = CreateObjectAbove(Grass, 229, 358);
-	var Grass030 = CreateObjectAbove(Grass, 208, 358);
+	CreateObjectAbove(Grass, 250, 356);
+	var Grass028 = CreateObjectAbove(Grass, 229, 358);
+	var Grass029 = CreateObjectAbove(Grass, 208, 358);
 
-	CreateObjectAbove(Deco_Tree_Deciduous4, 225, 361);
+	var Deco_Tree_Deciduous3001 = CreateObjectAbove(Deco_Tree_Deciduous3, 133, 355);
+	Deco_Tree_Deciduous3001->SetCon(102);
+	Deco_Tree_Deciduous3001->SetSeedAmount(0);
+	Deco_Tree_Deciduous3001->SetSeedChance(0);
 
-	CreateObjectAbove(Deco_Tree_Deciduous3, 141, 356);
-
-	var Tree_Deciduous001 = CreateObjectAbove(Tree_Deciduous, 124, 363);
+	var Tree_Deciduous001 = CreateObjectAbove(Tree_Deciduous, 153, 362);
 	Tree_Deciduous001->SetCon(102);
-
-	CreateObjectAbove(Tree_Coniferous2, 251, 364);
-	var Tree_Coniferous2001 = CreateObjectAbove(Tree_Coniferous2, 245, 364);
-	Tree_Coniferous2001->SetCon(101);
-
-	CreateObjectAbove(Tree_Coniferous4, 278, 344);
-
-	var Tree_Deciduous002 = CreateObjectAbove(Tree_Deciduous, 236, 363);
-	Tree_Deciduous002->SetCon(101);
-	var Tree_Deciduous003 = CreateObjectAbove(Tree_Deciduous, 271, 349);
-	Tree_Deciduous003->SetCon(101);
-	CreateObjectAbove(Tree_Deciduous, 183, 364);
-	CreateObjectAbove(Tree_Deciduous, 192, 363);
+	Tree_Deciduous001->SetSeedAmount(0);
+	Tree_Deciduous001->SetSeedChance(0);
 
 	CreateObjectAbove(EnvPack_Guidepost2, 90, 360);
 
-	var Flower001 = CreateObjectAbove(Flower, 160, 365);
-	var Flower002 = CreateObjectAbove(Flower, 169, 366);
-	var Flower003 = CreateObjectAbove(Flower, 197, 366);
-	var Flower004 = CreateObjectAbove(Flower, 251, 366);
-	var Flower005 = CreateObjectAbove(Flower, 208, 367);
+	CreateObjectAbove(Flower, 160, 365);
+	CreateObjectAbove(Flower, 169, 366);
+	var Flower001 = CreateObjectAbove(Flower, 197, 366);
+	var Flower002 = CreateObjectAbove(Flower, 253, 364);
+	var Flower003 = CreateObjectAbove(Flower, 208, 367);
 
 	var Wheat001 = CreateObjectAbove(Wheat, 188, 360);
 	Wheat001->SetMeshMaterial("wheat_material_ripe", 0);
 	var Wheat002 = CreateObjectAbove(Wheat, 179, 358);
 	Wheat002->SetMeshMaterial("wheat_material_ripe", 0);
-	var Flower006 = CreateObjectAbove(Flower, 127, 368);
+
+	CreateObjectAbove(Flower, 148, 345);
 	CreateObjectAbove(Flower, 229, 367);
 
-	var Tree_Coniferous2002 = CreateObjectAbove(Tree_Coniferous2, 280, 345);
-	Tree_Coniferous2002->SetCon(99);
-	var Tree_Coniferous2003 = CreateObjectAbove(Tree_Coniferous2, 136, 365);
-	Tree_Coniferous2003->SetCon(104);
-	var Tree_Coniferous2004 = CreateObjectAbove(Tree_Coniferous2, 257, 357);
-	Tree_Coniferous2004->SetCon(96);
-	CreateObjectAbove(Tree_Coniferous2, 163, 364);
+	var Tree_Coniferous2001 = CreateObjectAbove(Tree_Coniferous2, 259, 361);
+	Tree_Coniferous2001->SetCon(104);
+	Tree_Coniferous2001->SetSeedAmount(0);
+	Tree_Coniferous2001->SetSeedChance(0);
 
-	var Flower007 = CreateObjectAbove(Flower, 288, 342);
-	Flower007->SetCon(90);
+	var Flower004 = CreateObjectAbove(Flower, 288, 342);
+	Flower004->SetCon(97);
 
-	var Tree_Deciduous004 = CreateObjectAbove(Tree_Deciduous, 207, 361);
-	Tree_Deciduous004->SetCon(99);
+	var Tree_Coniferous4001 = CreateObjectAbove(Tree_Coniferous4, 202, 362);
+	Tree_Coniferous4001->SetCon(96);
+	Tree_Coniferous4001->SetSeedAmount(0);
+	Tree_Coniferous4001->SetSeedChance(0);
 
-	var Tree_Coniferous4001 = CreateObjectAbove(Tree_Coniferous4, 199, 361);
-	Tree_Coniferous4001->SetCon(95);
+	var Tree_Deciduous002 = CreateObjectAbove(Tree_Deciduous, 182, 363);
+	Tree_Deciduous002->SetCon(103);
+	Tree_Deciduous002->SetSeedAmount(0);
+	Tree_Deciduous002->SetSeedChance(0);
 
-	var Flower008 = CreateObject(Flower, 271, 350);
-	Flower008->SetCon(5);
+	var Tree_Coniferous2002 = CreateObjectAbove(Tree_Coniferous2, 269, 349);
+	Tree_Coniferous2002->SetCon(104);
+	Tree_Coniferous2002->SetSeedAmount(0);
+	Tree_Coniferous2002->SetSeedChance(0);
 
-	var Tree_Coniferous4002 = CreateObject(Tree_Coniferous4, 127, 366);
-	Tree_Coniferous4002->SetCon(5);
-	var Tree_Coniferous4003 = CreateObject(Tree_Coniferous4, 251, 366);
-	Tree_Coniferous4003->SetCon(1);
+	var Tree_Deciduous003 = CreateObjectAbove(Tree_Deciduous, 288, 338);
+	Tree_Deciduous003->SetCon(102);
+	Tree_Deciduous003->SetSeedAmount(0);
+	Tree_Deciduous003->SetSeedChance(0);
+
+	var Tree_Coniferous4002 = CreateObjectAbove(Tree_Coniferous4, 168, 361);
+	Tree_Coniferous4002->SetSeedAmount(0);
+	Tree_Coniferous4002->SetSeedChance(0);
+	var Tree_Coniferous4003 = CreateObjectAbove(Tree_Coniferous4, 175, 362);
+	Tree_Coniferous4003->SetCon(95);
+	Tree_Coniferous4003->SetSeedAmount(0);
+	Tree_Coniferous4003->SetSeedChance(0);
+
+	var Tree_Deciduous004 = CreateObjectAbove(Tree_Deciduous, 219, 363);
+	Tree_Deciduous004->SetCon(102);
+	Tree_Deciduous004->SetSeedAmount(0);
+	Tree_Deciduous004->SetSeedChance(0);
+
+	var Wheat003 = CreateObject(Wheat, 212, 371);
+	Wheat003->SetCon(9);
+	Wheat003->SetMeshMaterial("wheat_material_ripe", 0);
+	CreateObjectAbove(Fern, 531, 424);
+	CreateObjectAbove(Fern, 565, 415);
+
+	var Deco_Tree_Deciduous4001 = CreateObjectAbove(Deco_Tree_Deciduous4, 253, 362);
+	Deco_Tree_Deciduous4001->SetCon(101);
+	Deco_Tree_Deciduous4001.Plane = 101;
+	Deco_Tree_Deciduous4001->SetSeedAmount(0);
+	Deco_Tree_Deciduous4001->SetSeedChance(0);
 
 	CreateObjectAbove(RoomEntrance, 609, 303);
 
@@ -102,6 +122,7 @@ func InitializeObjects()
 	SpinWheel001->SetStoneDoor(StoneDoor001);
 
 	CreateObjectAbove(Ruin_WoodenCabin, 343, 335);
+
 	var Clonk001 = CreateObjectAbove(Clonk, 342, 335);
 	Clonk001->SetColor(0x813100);
 	Clonk001->SetMeshMaterial("Clonk_Sage", 0);
@@ -112,6 +133,48 @@ func InitializeObjects()
 	var Pickaxe001 = Clonk001->CreateContents(Pickaxe);
 
 	var Dialogue001 = Clonk001->SetDialogue("Editor",true);
-	Dialogue001->SetUserDialogue({Actions=[{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Good day stranger."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Hi there, what happened to your cabin?"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="There are some evil guys who took over the tower ahead and destroyed my little cabin."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="What is so special about the tower that they would destroy your tower?"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="I still have to find out. I am an archaeologist and was studing this ancient tower, it has several rooms with riddles and artefacts."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Oh that's interesting! I like riddles, how can I enter the tower?"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Well you just turn the lever ahead and go into the first room. The further you get the harder the riddles! I did not make it to the top before those evil guys came."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Okay, I am going to give it a try!"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Take care!"}}],Function="sequence"}, nil);
+	Dialogue001->SetUserDialogue({Actions=[{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Good day stranger."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Hi there, what happened to your cabin?"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="There are some evil guys who took over the tower ahead and destroyed my little cabin."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="What is so special about the tower that they would destroy your cabin?"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="I still have to find out. I am an archaeologist and was studying this ancient tower, it has several rooms with riddles and artifacts."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Oh that's interesting! I love riddles, how can I enter the tower?"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Well you just turn the lever ahead and go into the first room. The further you get the harder the riddles! I did not make it to the top before those evil guys came."}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="triggering_clonk"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Okay, I am going to give it a try!"}},{AfterMessage="next",Function="message",Options=[],Speaker={Function="npc"},TargetPlayers={Function="triggering_player_list"},Text={Function="string_constant",Value="Take care!"}}],Function="sequence"}, nil);
+
+	CreateObject(Rock, 108, 413);
+	CreateObject(Rock, 360, 374);
+	CreateObject(Rock, 434, 390);
+	CreateObject(Rock, 541, 467);
+	CreateObject(Rock, 575, 444);
+	CreateObject(Rock, 453, 460);
+	CreateObject(Rock, 243, 469);
+	CreateObject(Rock, 35, 443);
+
+	CreateObject(Coal, 579, 378);
+	CreateObject(Coal, 562, 359);
+	CreateObject(Coal, 567, 372);
+	CreateObject(Coal, 547, 374);
+
+	CreateObject(Ore, 106, 470);
+	CreateObject(Ore, 85, 461);
+	CreateObject(Ore, 229, 443);
+	CreateObject(Ore, 258, 413);
+
+	CreateObject(Loam, 519, 350);
+	CreateObject(Loam, 616, 419);
+	CreateObject(Loam, 311, 460);
+	CreateObject(Loam, 78, 397);
+	CreateObject(Loam, 391, 387);
+	CreateObject(Loam, 478, 457);
+
+	CreateObjectAbove(Mushroom, 225, 360);
+	CreateObjectAbove(Mushroom, 188, 361);
+	CreateObjectAbove(Mushroom, 545, 424);
+	CreateObjectAbove(Mushroom, 521, 423);
+
+	CreateObjectAbove(Seaweed, 396, 454);
+	CreateObjectAbove(Seaweed, 372, 454);
+
+	CreateObject(Firestone, 487, 371);
+	CreateObject(Firestone, 557, 449);
+	CreateObject(Firestone, 278, 466);
+	CreateObject(Firestone, 452, 437);
+	CreateObject(Firestone, 189, 400);
+	CreateObject(Firestone, 135, 430);
+	CreateObject(Firestone, 272, 390);
 	return true;
 }

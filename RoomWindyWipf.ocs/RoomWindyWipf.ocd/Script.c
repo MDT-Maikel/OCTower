62d5bf91cc7f7d288854bf167236f9e003cd7881
Maikel de Vries <mdt.maikel@gmail.com>
2016-10-03 10:13:08 +0200

    add room (windy wipf)

---
+++ RoomWindyWipf.ocs/RoomWindyWipf.ocd/Script.c
@@ -0,0 +1,58 @@
+/**
+	Room Control
+	This object's script contains the properties and the functions of the room.
+
+	@author See contents of GetRoomAuthor().
+*/
+
+#include RoomBase
+
+
+/*-- Room Properties --*/
+// Overload these properties if needed.
+
+// The name of the room, appears in menus and the room start.
+// Add both an English and German version.
+public func GetRoomName() { return "$RoomName$"; }
+
+// The description of the room, appears in menus and the room start.
+// Add both an English and German version.
+public func GetRoomDescription() { return "$RoomDescription$"; }
+
+// The name of the author of the room, separate multiple authors with comma's or & (like "John, Jane & Wipf").
+// Names may not contain comma's or &.
+public func GetRoomAuthor() { return "Sven2, Maikel"; }
+
+// The map section that needs to be loaded for this room, usually equal to the name of the room.
+public func GetRoomSection() { return "WindyWipf"; }
+
+// A unique ID for the room which is used for saving player progress. It must be two characters
+// long which can be chosen from the set [A-Za-z].
+public func GetRoomID() { return "WI"; }
+
+// The difficulty of this room, ranges from 0 to 999.
+public func GetRoomDifficulty() { return 100; }
+
+// Whether or not the room has an ancient tablet which can be found.
+public func HasTablet() { return false; }
+
+// Whether or not the room has a joker which can be found.
+public func HasJoker() { return false; }
+
+
+/*-- Room Scripts --*/
+
+// Called when the room is initialized.
+// Here you can create effects and extra objects that are needed for the room.
+public func OnRoomInit()
+{
+	FindObject(Find_ID(WindBag))->DoFullLoad();
+	return;
+}
+
+// Called when the player starts its room attempt.
+// Here you can create effects and extra objects that are needed for the player and its crew.
+public func OnPlayerInit(int plr)
+{	
+	return;
+}

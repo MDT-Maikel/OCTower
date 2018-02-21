2402f0c67d210aaf49715ec0c107a5a73b46f605
Maikel de Vries <mdt.maikel@gmail.com>
2018-02-21 18:09:31 +0100

    add general tower data statistics

---
+++ TowerObjects.ocd/Statistics.ocd/TowerData.ocd/Script.c
@@ -0,0 +1,29 @@
+/**
+	Statistics: Tower Data
+	Some general data for the tower.
+	
+	@author Maikel
+*/
+
+
+local stats;
+
+public func Initialize()
+{
+	stats = {};
+	// Store the version of the tower.
+	stats["TowerVersion"] = GetTowerVersion();
+	return;
+}
+
+public func CollectStats()
+{
+	var round_type = FindObject(Find_ID(Statistics_TowerData));
+	if (round_type)
+		return round_type->GetStats();
+	return nil;
+}
+
+public func GetStats() { return stats; }
+
+public func IsStatistics() { return true; }

69af59090a426744e898d3973ed35232e436160f
Maikel de Vries <mdt.maikel@gmail.com>
2018-02-22 17:52:21 +0100

    pseudonymize room rating statistics gathering

--- TowerObjects.ocd/Statistics.ocd/RoomRating.ocd/Script.c
+++ TowerObjects.ocd/Statistics.ocd/RoomRating.ocd/Script.c
@@ -20,9 +20,23 @@ public func OnRoomRatingChanged(id room, int by_plr, int rating)
 	var room_str = Format("%i", room);
 	if (!stats[room_str])
 		stats[room_str] = {};
-	var plr_str = GetPlayerName(by_plr);
+	var plr_str = GetPlayerRoomRatingID(by_plr);
 	stats[room_str][plr_str] = rating;
 	return;
+}
+
+public func GetPlayerRoomRatingID(int plr)
+{
+	// Get stored rating ID.
+	var rating_id = GetPlrExtraData(plr, "OCTowerPlayerRoomRatingID");
+	// Generate a rating ID if there is no one yet.
+	if (!rating_id)
+	{
+		// Create a random ID with the methods available to us.
+		rating_id = Format("%d%d%d%d", Random(2147483647), Random(2147483647), Random(2147483647), Random(2147483647));
+		SetPlrExtraData(plr, "OCTowerPlayerRoomRatingID", rating_id);
+	}
+	return rating_id;
 }
 
 public func CollectStats()

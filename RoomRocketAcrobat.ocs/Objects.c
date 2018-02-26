1914914aca00355451bb89c08a41b3e09915a695
Maikel de Vries <mdt.maikel@gmail.com>
2018-02-26 13:50:59 +0100

    room rocket acrobat: improve flow and joker collection

--- RoomRocketAcrobat.ocs/Objects.c
+++ RoomRocketAcrobat.ocs/Objects.c
@@ -2,13 +2,20 @@
 
 func InitializeObjects()
 {
+	var Torch001 = CreateObjectAbove(Torch, 530, 54);
+	Torch001->AttachToWall(false);
+	Torch001->SetMeshMaterial("Torch", 0);
+	Torch001->SetState(2);
+
 	var Trunk001 = CreateObject(Trunk, 600, 418);
 	Trunk001->SetCon(114);
 	Trunk001->SetR(-133);
 	Trunk001->SetPosition(600, 418);
-	var ItemSpawn001 = ItemSpawn->Create(Boompack,518,442);
-	ItemSpawn001->SetPosition(518, 442);
 
+	var ItemSpawn001 = ItemSpawn->Create(Boompack,511,443);
+	ItemSpawn001->SetPosition(511, 443);
+	var ItemSpawn002 = ItemSpawn->Create(Boompack,536,443);
+	ItemSpawn002->SetPosition(536, 443);
 	var RoomEntrance001 = CreateObjectAbove(RoomEntrance, 564, 455);
 	RoomEntrance001->SetColor(0xff32c832);
 
@@ -18,14 +25,18 @@ func InitializeObjects()
 	var RoomExit001 = CreateObjectAbove(RoomExit, 567, 63);
 	RoomExit001->SetColor(0xffc83232);
 
-	var MovingBrick001 = CreateObject(MovingBrick, 20, 307);
+	var MovingBrick001 = CreateObject(MovingBrick, 20, 298);
 	MovingBrick001->SetComDir(COMD_Down);
 	MovingBrick001->SetYDir(10);
 	MovingBrick001->MoveVertical(246, 466);
-	MovingBrick001->SetPosition(20, 307);
+	MovingBrick001->SetPosition(20, 298);
 
 	var Column001 = CreateObject(Column, 320, 320);
 	Column001->SetPosition(320, 320);
+
+	var Lorry001 = CreateObject(Lorry, 325, 315);
+	Lorry001->SetR(45);
+	Lorry001->SetPosition(325, 315);
 
 	var Clonk001 = CreateObjectAbove(Clonk, 269, 360);
 	Clonk001->SetDir(DIR_Right);
@@ -60,5 +71,8 @@ func InitializeObjects()
 
 	var Joker001 = CreateObjectAbove(Joker, 516, 343);
 	Joker001->Unstick(7);
+
+	var Dynamite001 = Lorry001->CreateContents(Dynamite);
+	Dynamite001->SetPosition(325, 315);
 	return true;
 }
